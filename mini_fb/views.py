cb41dd8e6979031b70873f63db631b5fe8e7cd2a
clbrown02 <chrislbr@bu.edu>
2025-02-20 22:11:28 -0500

    finished single view

--- mini_fb/views.py
+++ mini_fb/views.py
@@ -8,3 +8,10 @@ class ShowAllProfilesView(ListView):
   model = Profile
   template_name = "mini_fb/show_all_profiles.html"
   context_object_name = "profiles"
+
+class ShowProfilePageView(DetailView):
+  '''Define a show class to show one profile'''
+
+  model = Profile
+  template_name = "mini_fb/show_profile.html"
+  context_object_name = "profile"
