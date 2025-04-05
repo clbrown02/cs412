bac7d91928841db3ca006075ed0206b09af27599
clbrown02 <chrislbr@bu.edu>
2025-04-04 20:44:08 -0400

    implement 3

--- voter_analytics/urls.py
+++ voter_analytics/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
   path(r'', views.VoterResultsView.as_view(), name='voter_home'),
   path(r'results', views.VoterResultsView.as_view(), name='voter_results_list'),
-  path('voter/<int:pk>/', views.VoterDetailView.as_view(), name='voter'),
+  path(r'voter/<int:pk>/', views.VoterDetailView.as_view(), name='voter'),
+  path(r'graphs', views.GraphView.as_view(), name = 'voter_graph')
 ]
--- voter_analytics/views.py
+++ voter_analytics/views.py
@@ -3,6 +3,12 @@ from django.db.models.query import QuerySet
 from django.views.generic import ListView, DetailView
 from . models import Voter
 import datetime
+import pandas as pd
+import plotly.express as px
+from django.db.models import Count
+from django.db.models.functions import ExtractYear
+
+
 
 # Create your views here.
 class VoterResultsView(ListView):
@@ -64,3 +70,74 @@ class VoterDetailView(DetailView):
     model = Voter
     template_name = 'voter_analytics/voter_detail.html'
     context_object_name = 'voter'
+
+class GraphView(ListView):
+   '''Class to display voter analytics in a graph/chart'''
+
+   model = Voter
+   template_name = 'voter_analytics/graphs.html'
+   context_object_name = 'voters'
+   paginate_by = 100
+
+   def get_context_data(self, **kwargs):
+      '''Method to obtain context_data'''
+
+      context = super().get_context_data(**kwargs)
+
+      birth_qs = Voter.objects.annotate(year=ExtractYear('dob')).values('year').annotate(count=Count('id')).order_by('year')
+      df_birth = pd.DataFrame(list(birth_qs))
+      if not df_birth.empty:
+          fig_birth = px.bar(
+              df_birth,
+              x='year',
+              y='count',
+              title='Distribution of Voters by Year of Birth',
+              labels={'year': 'Year of Birth', 'count': 'Number of Voters'}
+          )
+          birth_chart_html = fig_birth.to_html(full_html=False)
+      else:
+          birth_chart_html = "<p>No data available</p>"
+      context['birth_chart_html'] = birth_chart_html
+
+
+      party_qs = Voter.objects.values('party').annotate(count=Count('id'))
+      df_party = pd.DataFrame(list(party_qs))
+      if not df_party.empty:
+          fig_party = px.pie(
+              df_party,
+              names='party',
+              values='count',
+              title='Voter Party Affiliation'
+          )
+          party_chart_html = fig_party.to_html(full_html=False)
+      else:
+          party_chart_html = "<p>No data available</p>"
+      context['party_chart_html'] = party_chart_html
+
+      
+      election_counts = {
+          'v20': Voter.objects.filter(v20__iexact="TRUE").count(),
+          'v21_t': Voter.objects.filter(v21_t__iexact="TRUE").count(),
+          'v21_p': Voter.objects.filter(v21_p__iexact="TRUE").count(),
+          'v22': Voter.objects.filter(v22__iexact="TRUE").count(),
+          'v23': Voter.objects.filter(v23__iexact="TRUE").count(),
+      }
+      df_election = pd.DataFrame(list(election_counts.items()), columns=['Election', 'Count'])
+      if not df_election.empty:
+          fig_election = px.bar(
+              df_election,
+              x='Election',
+              y='Count',
+              title='Voter Participation by Election',
+              labels={'Election': 'Election', 'Count': 'Number of Voters'}
+          )
+          election_chart_html = fig_election.to_html(full_html=False)
+      else:
+          election_chart_html = "<p>No data available</p>"
+      context['election_chart_html'] = election_chart_html
+
+      current_year = datetime.date.today().year
+      context['years'] = list(range(1900, current_year + 1))
+      context['score_range'] = list(range(0, 5))
+
+      return context
